2f8a4fd449829deee08bc64d4a6199dd7d546f9c
Suhas HM <vksuhas2016@gmail.com>
2022-04-27 20:50:44 +0530

    first

---
+++ src/pizzadata.js
@@ -0,0 +1,31 @@
+const pizzas =[
+    {
+        name:"PEPPER BARBEQUE CHICKEN",
+        varients:["small","medium","large"],
+        prices:[
+            {
+                small:199,
+                medium:299,
+                large:399,
+            },
+        ],
+        category:"nonveg",
+        image:"https://images.dominos.co.in/new_veggie_paradise.jpg",
+        description:"Pepper Barbeque Chicken I Cheese"
+    },
+    {
+        name:"Jalpeno & Red Paprika Pizza",
+        varients:["small","medium","large"],
+        prices:[
+            {
+                small:149,
+                medium:249,
+                large:349,
+            },
+        ],
+        category:"veg",
+        image:"https://www.crazymasalafood.com/wp-content/images/jalepeno.jpg",
+        description:"very good pizza"
+    }
+];
+export default pizzas
---
+++ src/screens/Homescreens.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import pizzas from '../pizzadata'
+import Pizza from '../components/Pizza'
+export default function Pizzas() {
+  return (
+    <div>
+      <div className="row">
+          {pizzas.map(pizza=>{
+              return <div className='col-md-4'>
+                  <div>
+                      <Pizza pizza={pizza}/>
+                  </div>
+                  </div>
+          })
+          }
+      </div>
+    </div>
+  )
+}
